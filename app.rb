1732690cf88f9bec8060073312419ae0d5676357
Johan Lundahl <yohan.lundahl@gmail.com>
2014-02-07 16:40:03 +0100

    Shows ongoing and finished game data.

--- app.rb
+++ app.rb
@@ -25,12 +25,38 @@ class App < Sinatra::Base
 
     @messages_received = 0
 
+    @ongoing_games = {}
+    @finished_games = {}
+
+    @node_id = SecureRandom.uuid
+
     @queue.bind(@exchange, routing_key: '#').subscribe do |header, payload|
       @messages_received += 1
+
       $log.debug "---------- received message ----------"
       $log.debug "content type: #{header.content_type}"
       $log.debug "headers: #{header.headers}"
       $log.debug "payload: #{payload}"
+
+      stream_id = header.headers['streamId']
+
+      if header.type == 'GameCreatedEvent'
+        log "Added new game - #{stream_id}"
+        game = JSON.parse(payload) rescue 'bad payload'
+        @ongoing_games[stream_id] = game
+      elsif header.type == 'GameEndedEvent'
+        if @ongoing_games.has_key? stream_id
+          log "Known game has ended - #{stream_id}"
+          game = JSON.parse(payload) rescue 'bad payload'
+          ongoing_game = @ongoing_games.delete(stream_id)
+
+          if game.is_a?(Hash) && ongoing_game.is_a?(Hash)
+            game.merge! ongoing_game
+          end
+
+          @finished_games[stream_id] = game
+        end
+      end
     end
 
     channel.on_error do |ch, channel_close|
@@ -40,6 +66,13 @@ class App < Sinatra::Base
     service_up
   end
 
+  def log(msg, level = 'DEBUG')
+    publish({
+      message: msg,
+      level: level,
+    }, APP_ID, @node_id, 'LogEvent', 'log')
+  end
+
   def service_up
     publish({
       createdBy: "Johan Lundahl",
@@ -47,6 +80,7 @@ class App < Sinatra::Base
       sourceUrl: "https://github.com/quezacoatl/micro-service-lab",
       serviceUrl: "http://young-crag-4311.herokuapp.com"
     }.to_json, APP_ID, APP_ID, 'ServiceOnlineEvent', 'service')
+    log "#{APP_ID} is up and running!"
   end
 
   def service_down
@@ -67,6 +101,13 @@ class App < Sinatra::Base
   end
 
   get '/' do
-    "Number of messages received: #{@messages_received}"
+    content_type 'application/json'
+    {
+      messages_received: @messages_received,
+      games: {
+        ongoing: @ongoing_games,
+        finished: @finished_games
+      }
+    }.to_json
   end
 end
