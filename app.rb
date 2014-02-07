175750a42e3e6c88bdea649d7ecedb127bb9ef1a
Johan Lundahl <yohan.lundahl@gmail.com>
2014-02-07 17:15:43 +0100

    Fixed timezone problem for dinner countdown.

--- app.rb
+++ app.rb
@@ -16,11 +16,37 @@ class Numeric
     days  = hours / 24
 
     if days > 0
-      "#{days} days and #{hours % 24} hours"
+      hour_remainder = hours % 24
+      if hour_remainder > 0
+        hour_str = hour_remainder == 1 ? 'hour' : 'hours'
+        "#{days} days and #{hour_remainder} #{hour_str}"
+      elsif days == 1
+        "#{days} day"
+      else
+        "#{days} days"
+      end
     elsif hours > 0
-      "#{hours} hours and #{mins % 60} minutes"
+      min_remainder = mins % 60
+      if min_remainder > 0
+        min_str = min_remainder == 1 ? 'minute' : 'minutes'
+        "#{hours} hours and #{min_remainder} #{min_str}"
+      elsif hours == 1
+        "#{hours} hour"
+      else
+        "#{hours} hours"
+      end
     elsif mins > 0
-      "#{mins} minutes and #{secs % 60} seconds"
+      sec_remainder = secs % 60
+      if sec_remainder > 0
+        sec_str = sec_remainder == 1 ? 'second' : 'seconds'
+        "#{mins} minutes and #{sec_remainder} #{sec_str}"
+      elsif minutes == 1
+        "#{mins} minute"
+      else
+        "#{mins} minutes"
+      end
+    elsif secs == 1
+      "#{secs} second"
     elsif secs >= 0
       "#{secs} seconds"
     end
@@ -57,23 +83,26 @@ class App < Sinatra::Base
       $log.debug "headers: #{header.headers}"
       $log.debug "payload: #{payload}"
 
-      stream_id = header.headers['streamId']
+      if header
 
-      if header.type == 'GameCreatedEvent'
-        log "Added new game - #{stream_id}"
-        game = JSON.parse(payload) rescue 'bad payload'
-        @ongoing_games[stream_id] = game
-      elsif header.type == 'GameEndedEvent'
-        if @ongoing_games.has_key? stream_id
-          log "Known game has ended - #{stream_id}"
+        stream_id = header.headers['streamId']
+
+        if header.type == 'GameCreatedEvent'
+          log "Added new game - #{stream_id}"
           game = JSON.parse(payload) rescue 'bad payload'
-          ongoing_game = @ongoing_games.delete(stream_id)
+          @ongoing_games[stream_id] = game
+        elsif header.type == 'GameEndedEvent'
+          if @ongoing_games.has_key? stream_id
+            log "Known game has ended - #{stream_id}"
+            game = JSON.parse(payload) rescue 'bad payload'
+            ongoing_game = @ongoing_games.delete(stream_id)
 
-          if game.is_a?(Hash) && ongoing_game.is_a?(Hash)
-            game.merge! ongoing_game
+            if game.is_a?(Hash) && ongoing_game.is_a?(Hash)
+              game.merge! ongoing_game
+            end
+
+            @finished_games[stream_id] = game
           end
-
-          @finished_games[stream_id] = game
         end
       end
     end
@@ -85,11 +114,11 @@ class App < Sinatra::Base
     service_up
 
     EM.add_periodic_timer(10) do
-      time_diff = Time.new(2014,02,07,18,30) - Time.now
+      time_diff = Time.gm(2014,02,07,17,30) - Time.now
       if time_diff < 0
         log "It's time for dinner!"
       else
-        log "It's dinner in #{time_diff.duration}."
+        puts "It's dinner in #{time_diff.duration}."
       end
     end
   end
